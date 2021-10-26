935284a8ada0847ecce34c66946dcfcb94505a97
lucawic <lucaswick@email.arizona.edu>
2021-10-26 08:23:54 -0700

    starting About and Projects pages

--- reactportfolio/src/index.js
+++ reactportfolio/src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+//import components
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
+
+function App() {
+  return (
+    <div>
+      <h1>My component</h1>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.Fragment>
     <App />
-  </React.StrictMode>,
+    <About />
+    <Projects />
+  
+  </React.Fragment>,
   document.getElementById('root')
 );
