d96276993c530b8252d93bfed759e6b4e149ad52
lucawic <lucaswick@email.arizona.edu>
2021-10-30 07:19:52 -0700

    attempting footer

--- reactportfolio/src/index.js
+++ reactportfolio/src/index.js
@@ -9,6 +9,7 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
